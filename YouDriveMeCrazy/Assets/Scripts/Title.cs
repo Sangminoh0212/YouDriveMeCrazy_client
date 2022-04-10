ba3a6677cb4cb03d8a83f444a5ec353b1d58dc78
Ahnsihun <ahnsihun9208@gmail.com>
2022-04-10 23:49:55 +0900

    Title Screen
    타이틀 스크린을 약간 구현했습니다

---
+++ YouDriveMeCrazy/Assets/Scripts/Title.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Title : MonoBehaviour
+{
+     public string SceneToLoad;
+    
+     void Start() {
+        
+    }
+     void Update() {
+        
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+// public void QuitGame()
+//     {
+//         Application.quitting;
+//     }
+}
