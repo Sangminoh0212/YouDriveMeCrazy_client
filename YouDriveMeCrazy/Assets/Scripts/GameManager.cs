197ccbb391ee838350365ee941704641d7f63e35
Sangminoh0212 <ohsm1226@naver.com>
2022-05-11 13:11:41 +0900

    make stage1,stage2

--- YouDriveMeCrazy/Assets/Scripts/GameManager.cs
+++ YouDriveMeCrazy/Assets/Scripts/GameManager.cs
@@ -18,9 +18,11 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     #region UI
     private GameObject ui;
-    private GameObject stageClearPanel;
-    private GameObject gameOverPanel;
+    [SerializeField] private GameObject stageClearPanel;
+    [SerializeField] private GameObject gameOverPanel;
     #endregion
+
+    private static int clearNum;
 
     private int time;
 
@@ -37,34 +39,44 @@ public class GameManager : MonoBehaviourPunCallbacks
     void Start()
     {
         Setup();
-        PhotonNetwork.Instantiate(inputManager.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        //PhotonNetwork.Instantiate(inputManager.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
     }
 
 
     // by 상민, stageClear, gameOver Panel 모두 SetAvtive(false)
     void Setup() {
-        if(GameObject.Find("InputManager")) inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
+        if (GameObject.Find("InputManager")) inputManager = GameObject.Find("InputManager").GetComponent<InputManager>();
 
-        if(GameObject.Find("UI")){
-            stageClearPanel = GameObject.Find("UI").transform.GetChild(0).gameObject;
-            gameOverPanel = GameObject.Find("UI").transform.GetChild(1).gameObject;
-        }
-        if(stageClearPanel != null) stageClearPanel.SetActive(false);
+        if (stageClearPanel != null) stageClearPanel.SetActive(false);
         if (gameOverPanel != null) gameOverPanel.SetActive(false);
     }
     
     public void GameOver(){
         print("game over!!");
-        StartCoroutine(CallPolice());
+        StartCoroutine(CallGameOver());
+    }
+
+    public void StageClear(){
+        clearNum++;
+        print("stage clear!!");
+        // by상민, clearNum==2시 스코어보드 불러야함
+        if(clearNum==2) { StartCoroutine(CallStageClear()); } else { StartCoroutine(CallStageClear()); }
     }
 
     // by 상민, 플레이어 뒤에서 경찰차 소환 추가 필요
     // 다른 클래스에서 GameManager.Instance.GameOver() 호출해서 게임 오버 시키면 경찰차 등장 후 @초 후에 GameOverPanel 활성화
-    private IEnumerator CallPolice()
+    public IEnumerator CallGameOver()
     {
         // by 상민, 경찰차 소환 필요
         yield return new WaitForSeconds(2f);
-        if(gameOverPanel != null) gameOverPanel.SetActive(true);
+        if(gameOverPanel != null) 
+            gameOverPanel.SetActive(true);
+    }
+
+    public IEnumerator CallStageClear(){
+        yield return new WaitForSeconds(2f);
+        if (stageClearPanel != null) 
+        {stageClearPanel.SetActive(true); }
     }
 
 
@@ -83,14 +95,14 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     // by 상민, 버튼 누른 사람이 방장&&현재 참가자 두명일 때 만 게임 재시작 가능
     // photonView.RPC 를 이용해 Master, client 모두 RestartStage1Scene() 호출
-    public void Clear()
+    public void Next()
     {
         if (PhotonNetwork.IsMasterClient)
         {
             if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
             {
                 PhotonView photonView = PhotonView.Get(this);
-                photonView.RPC("ClearStage", RpcTarget.All);
+                photonView.RPC("SyncClearStage", RpcTarget.All);
             }
         }
     }
@@ -102,7 +114,7 @@ public class GameManager : MonoBehaviourPunCallbacks
         if (PhotonNetwork.IsMasterClient){
             if (PhotonNetwork.CurrentRoom.PlayerCount == 2){
                 PhotonView photonView = PhotonView.Get(this);
-                photonView.RPC("RestartStage", RpcTarget.All);
+                photonView.RPC("SyncRestartStage", RpcTarget.All);
             }
         }
     }
@@ -116,15 +128,16 @@ public class GameManager : MonoBehaviourPunCallbacks
             if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
             {
                 PhotonView photonView = PhotonView.Get(this);
-                photonView.RPC("LeaveStage", RpcTarget.All);
+                photonView.RPC("SyncLeaveStage", RpcTarget.All);
             }
         }
     }
 
+
     #region ChangeScene
     // by 상민, 새로운 씬 로드하기 전 현재 오브젝트 제거
     [PunRPC]
-    private void ClearStage()
+    private void SyncNextStage()
     {
         PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer.ActorNumber);
         PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().buildIndex+1);
@@ -132,7 +145,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     // by 상민, 새로운 씬 로드하기 전 현재 오브젝트 제거
     [PunRPC]
-    private void RestartStage()
+    private void SyncRestartStage()
     {
         PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer.ActorNumber);
         PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().name);
@@ -140,7 +153,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     // by 상민, 새로운 씬 로드하기 전 현재 오브젝트 제거
     [PunRPC]
-    private void LeaveStage(){
+    private void SyncLeaveStage(){
         PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer.ActorNumber);
         PhotonNetwork.LoadLevel(0);
     }
